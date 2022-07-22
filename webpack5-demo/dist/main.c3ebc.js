6750bca47cba9c7de79290d4c0bb0d183126dceb
mengyang <2402334261@qq.com>
2022-07-22 09:26:23 +0800

    up

--- webpack5-demo/dist/main.c3ebc.js
+++
@@ -1,6 +0,0 @@
-(() => {
-    "use strict";
-    const n = { handler1: () => n.handler2(), handler2: () => "handler2" },
-        e = n;
-    (() => { const n = e.handler1(); console.log("init" + n) })()
-})();
